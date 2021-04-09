8dda5089b744106df67398ffb1a74b49476099d8
user <user@user-PC>
2021-04-09 14:43:42 +0700

    Menambah source code pada file Car.java

--- src/tugas/ke/pkg2/meet/pkg7/Car.java
+++ src/tugas/ke/pkg2/meet/pkg7/Car.java
@@ -10,5 +10,58 @@ package tugas.ke.pkg2.meet.pkg7;
  * @author ahmad nur hidaya
  */
 public class Car extends Transportation{
+    private int car1;
+    private int car2;
+    
+    public Car(){
+        this.car1 = 1;
+        this.car2 = 1;
+    }
+    
+    //Meminjam Mobil
+    public void Car1(){
+       // Car car1 = new Car();
+       if(car1 == Car1R){
+           this.car1 -= 1;
+           //Car1 sekarang = 0
+       }else if(car1 == Car1NR){
+           //Mobil telah dipipnjam orang lain
+       }        
+    }
+    public void Car2(){
+       // Car car2 = new Car();
+       if(car2 == Car2R){
+           this.car2 -= 1;
+           //Car2 sekarang = 0
+       }else if(car2 == Car2NR){
+           //Mobil telah dipipnjam orang lain
+       }else{
+           
+       }        
+    }
+    
+    //Mengembalikan Mobil
+    public void rePayCar1(){
+        if(car1 == Car1NR){
+            this.car1 += 1;
+            //Mobil sekarang = 1
+        }else if(car1 == Car1R){
+            //Mobil masih Ready
+        }else{
+            
+        }
+    }
+    public void rePayCar2(){
+        if(car2 == Car2NR){
+            this.car2 += 1;
+            //Mobil sekarang = 1
+        }else if(car2 == Car2R){
+            //mobil masih ready
+        }
+    }
+    public void ShowCarReady(){
+        System.out.println("Car 1 :"+this.car1+"| Car 2 :"+this.car2);
+    }
     
 }
+
